d14844f70712fb45a5e404c233c17c0327ba6bdf
torum <37834607+torum@users.noreply.github.com>
2023-01-01 16:43:35 +0900

    changed package to "selfcontained"

---
+++ BitWallpaper/ViewModels/ViewModelBase.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+
+namespace BitWallpaper.ViewModels;
+
+public abstract class ViewModelBase : INotifyPropertyChanged, IDataErrorInfo
+{
+    public ViewModelBase() { }
+
+    #region == INotifyPropertyChanged ==
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void NotifyPropertyChanged(string propertyName)
+    {
+        //this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        bool? uithread = (App.Current as App)?.CurrentDispatcherQueue?.HasThreadAccess;
+
+        if (uithread != null)
+        {
+            if (uithread == true)
+            {
+                DoNotifyPropertyChanged(propertyName);
+            }
+            else
+            {
+                (App.Current as App)?.CurrentDispatcherQueue?.TryEnqueue(() =>
+                {
+                    DoNotifyPropertyChanged(propertyName);
+                });
+            }
+        }
+
+        /*
+        (App.Current as App)?.TheSynchronizationContext?.Post(d => {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }, null);
+        */
+    }
+
+    private void DoNotifyPropertyChanged(string propertyName)
+    {
+        try
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Exception at NotifyPropertyChanged ({propertyName}): " + ex.Message);
+
+            (App.Current as App).AppendErrorLog($"Exception at NotifyPropertyChanged ({propertyName}): ", ex.Message);
+        }
+    }
+
+    #endregion
+
+    #region == IDataErrorInfo ==
+
+    private Dictionary<string, string> _ErrorMessages = new Dictionary<string, string>();
+
+    string IDataErrorInfo.Error
+    {
+        get
+        {
+            return (_ErrorMessages.Count > 0) ? "Has Error" : null;
+        }
+    }
+
+    string IDataErrorInfo.this[string columnName]
+    {
+        get
+        {
+            if (_ErrorMessages.ContainsKey(columnName))
+                return _ErrorMessages[columnName];
+            else
+                return "";
+        }
+    }
+
+    protected void SetError(string propertyName, string errorMessage)
+    {
+        _ErrorMessages[propertyName] = errorMessage;
+    }
+
+    protected void ClearErrror(string propertyName)
+    {
+        if (_ErrorMessages.ContainsKey(propertyName))
+            //_ErrorMessages.Remove(propertyName);
+            _ErrorMessages[propertyName] = "";
+    }
+
+
+    #endregion
+
+}
---
+++ BitWallpaper/Views/MonaJpyPage.xaml.cs
@@ -0,0 +1,60 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using BitWallpaper.ViewModels;
+using System.Diagnostics;
+using BitWallpaper.Models;
+using Microsoft.UI.Xaml.Markup;
+
+namespace BitWallpaper.Views
+{
+    public sealed partial class MonaJpyPage : Page
+    {
+        private PairViewModel _viewModel;
+
+        public PairViewModel ViewModel
+        {
+            get=> _viewModel;
+        }
+
+        public MonaJpyPage()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (XamlParseException parseException)
+            {
+                Debug.WriteLine($"Unhandled XamlParseException in MonaJpyPage: {parseException.Message}");
+                foreach (var key in parseException.Data.Keys)
+                {
+                    Debug.WriteLine("{Key}:{Value}", key.ToString(), parseException.Data[key]?.ToString());
+                }
+                throw;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //base.OnNavigatedTo(e);
+
+            if (e.Parameter is PairViewModel)
+            {
+                _viewModel = (PairViewModel)e.Parameter;
+            }
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
